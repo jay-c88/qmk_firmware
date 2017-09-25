1a6fb1e6a03c56e07d2b7112570c2d377dbb6d11
jay-c88 <jay-c88@users.noreply.github.com>
2017-09-25 03:03:49 +0200

    settings and first layers

--- keyboards/jc_icarus/jc_icarus.h
+++ keyboards/jc_icarus/jc_icarus.h
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef %KEYBOARD_UPPERCASE%_H
-#define %KEYBOARD_UPPERCASE%_H
+#ifndef JC_ICARUS_H
+#define JC_ICARUS_H
 
 #include "quantum.h"
 
@@ -23,12 +23,20 @@
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
 #define KEYMAP( \
-    k00, k01, k02, \
-      k10,  k11   \
+	k11, k21, k31, k41, k51, k61,							k26, k36, k46, k56, k66, k76, \
+	k12, k22, k32, k42, k52, k62,							k27, k37, k47, k57, k67, k77, \
+	k13, k23, k33, k43, k53, k63, k71, k72,		  k17, k16, k28, k38, k48, k58, k68, k78, \
+	k14, k24, k34, k44, k54, k64, k73, k74,		  k19, k18, k29, k39, k49, k59, k69, k79, \
+	k15, k25, k35, k45, 	 k55, k65, k75,		  k1a, k2a,      k3a, k4a, k5a, k6a, k7a, \
 ) \
 { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k11 }, \
+    { k11, k12, k13, k14, k15, k16, k17, k18, k19, k1a }, \
+    { k21, k22, k23, k24, k25, k26, k27, k28, k29, k2a }, \
+    { k31, k32, k33, k34, k35, k36, k37, k38, k39, k3a }, \
+    { k41, k42, k43, k44, k45, k46, k47, k48, k49, k4a }, \
+    { k51, k52, k53, k54, k55, k56, k57, k58, k59, k5a }, \
+    { k61, k62, k63, k64, k65, k66, k67, k68, k69, k6a }, \
+    { k71, k72, k73, k74, k75, k76, k77, k78, k79, k7a }, \
 }
 
 #endif
--- keyboards/jc_icarus/keymaps/default/keymap.c
+++ keyboards/jc_icarus/keymaps/default/keymap.c
@@ -13,13 +13,88 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include "%KEYBOARD%.h"
+#include "jc_icarus.h"
+#include "keymap_extras/keymap_german.h"
+#include "led.h"
+
+
+enum jc_layers {
+  _MAIN,    // Main Layer
+  _COLE,    // Colemak Layer (Toggle)
+  _SYMB,    // Symbol Layer (Momentary)
+  _NAVI,    // Navigation Layer (Momentary)
+  _NUMP,    // Numpad Layer (Momentary)
+  _MIRR,    // Mirror Layer (Momentary)
+  _CMIR,    // Colemak Mirror Layer (Momentary)
+  _LHUB,    // Layer Hub (Momentary)
+  _QMKL,    // QMK Layer (Toggle)
+  _MEDI,    // Media Layer (Toggle)
+};
+
+enum jc_customplanck_keycodes {
+  MAIN = SAFE_RANGE,
+  COLE,
+  SYMB,
+  NAVI,
+  NUMP,
+  MIRR,
+  CMIR,
+  LHUB,
+  QMKL,
+  MEDI,
+  DYNAMIC_MACRO_RANGE,  // has to be last entry!
+};
+
+#include "dynamic_macro.h"  // has to be after keycodes enum
+
+
+enum jc_customplanck_macros {
+  _M_MicM,
+};
+
+#define ________    KC_TRNS
+#define XXXXXXXX    KC_NO
+#define DE_MICR     ALGR(KC_M)
+
+#define DM_REC1     DYN_REC_START1
+#define DM_REC2     DYN_REC_START2
+#define DM_PLY1     DYN_MACRO_PLAY1
+#define DM_PLY2     DYN_MACRO_PLAY2
+#define DM_STOP     DYN_REC_STOP
+
+#define M_MicM      M(_M_MicM)
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = KEYMAP( /* Base */
-  KC_A,  KC_1,  KC_H, \
-    KC_TAB,  KC_SPC   \
+
+// _MAIN: Main Layer (QWERZ-DE)
+[_MAIN] = KEYMAP(
+    KC_ESC,    DE_1,    DE_2,    DE_3,    DE_4,    DE_5,                                            DE_6,    DE_7,    DE_8,    DE_9,    DE_0, KC_BSPC,
+    KC_TAB,    DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,                                            DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,   DE_SS,
+   KC_CAPS,    DE_A,    DE_S,    DE_D,    DE_F,    DE_G,  KC_ENT,    LHUB,    XXXXXXXX,  KC_ENT,    DE_H,    DE_J,    DE_K,    DE_L,   DE_OE,   DE_AE,
+   KC_LSFT,    DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,  KC_DEL, KC_BSPC,        MIRR,  KC_DEL,    DE_N,    DE_M, DE_COMM,  DE_DOT,   DE_UE, KC_RSFT,
+   KC_LCTL, KC_LGUI, KC_LALT,    NAVI,           KC_SPC,    MIRR,    NUMP,        NUMP,    SYMB,           KC_SPC,    NAVI, KC_RALT, KC_RGUI, KC_RCTL,
 ),
+
+// _COLE: Colemak Layer (Custom-DE)
+[_COLE] = KEYMAP(
+    KC_ESC,    DE_1,    DE_2,    DE_3,    DE_4,    DE_5,                                            DE_6,    DE_7,    DE_8,    DE_9,    DE_0, KC_BSPC,
+    KC_TAB,    DE_Q,    DE_W,    DE_F,    DE_P,    DE_G,                                            DE_J,    DE_L,    DE_U,    DE_Z,   DE_OE,   DE_SS,
+   KC_CAPS,    DE_A,    DE_R,    DE_S,    DE_T,    DE_D,  KC_ENT,    LHUB,    XXXXXXXX,  KC_ENT,    DE_H,    DE_N,    DE_E,    DE_I,    DE_O,   DE_AE,
+   KC_LSFT,    DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,  KC_DEL, KC_BSPC,        CMIR,  KC_DEL,    DE_K,    DE_M, DE_COMM,  DE_DOT,   DE_UE, KC_RSFT,
+   KC_LCTL, KC_LGUI, KC_LALT,    NAVI,           KC_SPC,    CMIR,    NUMP,        NUMP,    SYMB,           KC_SPC,    NAVI, KC_RALT, KC_RGUI, KC_RCTL,
+),
+
+
+
+// [_PLACEHOLDER] = KEYMAP(
+//   XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                                       ,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+//   XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,                                       ,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+//   XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,    XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+//   XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,    XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+//   XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,         XXXXXXXX,XXXXXXXX,XXXXXXXX,    XXXXXXXX,XXXXXXXX,         XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,XXXXXXXX,
+// ),
+
 };
 
 const uint16_t PROGMEM fn_actions[] = {
@@ -30,17 +105,120 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
       switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
+        case _M_MicM:
+          return MACRODOWN(D(CAPS), T(GRAVE), U(CAPS), END);
+          break;
       }
     return MACRO_NONE;
 };
 
+void persistant_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MAIN:
+        if (record->event.pressed) {
+          persistant_default_layer_set(1UL<<_MAIN);
+        }
+        return false;
+        break;
+    case COLE:
+        if (record->event.pressed) {
+          persistant_default_layer_set(1UL<<_COLE);
+        }
+        return false;
+        break;
+    case SYMB:
+        if (record->event.pressed) {
+            layer_on(_SYMB);
+        }
+        else {
+            layer_off(_SYMB);
+        }
+        return false;
+        break;
+    case NAVI:
+        if (record->event.pressed) {
+            layer_on(_NAVI);
+        }
+        else {
+            layer_off(_NAVI);
+        }
+        return false;
+        break;
+    case NUMP:
+        if (record->event.pressed) {
+            layer_on(_NUMP);
+        }
+        else {
+            layer_off(_NUMP);
+        }
+        return false;
+        break;
+    case MIRR:
+        if (record->event.pressed) {
+            layer_on(_MIRR);
+        }
+        else {
+            layer_off(_MIRR);
+        }
+        return false;
+        break;
+    case CMIR:
+        if (record->event.pressed) {
+            layer_on(_CMIR);
+        }
+        else {
+            layer_off(_CMIR);
+        }
+        return false;
+        break;
+    case LHUB:
+        if (record->event.pressed) {
+            layer_on(_LHUB);
+        }
+        else {
+            layer_off(_LHUB);
+        }
+        return false;
+        break;
+    case MEDI:
+        if (record->event.pressed) {
+            if(IS_LAYER_OFF(_MEDI)) {
+              layer_on(_MEDI);
+              layer_off(_LHUB);
+            }
+            else if(IS_LAYER_ON(_MEDI)) {
+              layer_off(_MEDI);
+              layer_on(_LHUB);
+            }
+        }
+        return false;
+        break;
+    case QMKL:
+        if (record->event.pressed) {
+            if(IS_LAYER_OFF(_QMKL)) {
+              layer_on(_QMKL);
+              layer_off(_LHUB);
+            }
+            else if(IS_LAYER_ON(_QMKL)) {
+              layer_off(_QMKL);
+              layer_on(_LHUB);
+            }
+        }
+        return false;
+        break;
+  }
+
+  if (!process_record_dynamic_macro(keycode, record)) {
+    return false;
+  }
+  
+  return true;
+}
 
 void matrix_init_user(void) {
 
@@ -48,10 +226,6 @@ void matrix_init_user(void) {
 
 void matrix_scan_user(void) {
 
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  return true;
 }
 
 void led_set_user(uint8_t usb_led) {
